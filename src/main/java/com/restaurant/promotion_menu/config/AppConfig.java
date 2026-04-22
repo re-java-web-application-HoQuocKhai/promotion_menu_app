4d39686e393781f8dd6f3ca653b8a86466f81385
HoQuocKhai <HoQuocKhai@users.noreply.github.com>
2026-04-22 21:10:03 +0700

    Fix lỗi

--- src/main/java/com/restaurant/promotion_menu/config/AppConfig.java
+++ src/main/java/com/restaurant/promotion_menu/config/AppConfig.java
@@ -49,7 +49,7 @@ public class AppConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/resources/")
-                .addResourceLocations("/resources/");
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations("/WEB-INF/resources/");
     }
 }
